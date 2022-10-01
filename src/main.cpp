4ea40f97aa33a3fb98de47758c1748ed9adfcd47
PavelKosnar <kosnarpaveljr@gmail.com>
2022-10-01 21:21:15 +0200

    Vytvoreni webserveru

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,51 @@
 #include <ESP8266WiFi.h>
-#define PIN 5
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FS.h>
 
-const char* ssid     = "KM";
-const char* password = "Pavel9317";
+
+const char* ssid     = "jmeno";
+const char* password = "heslo";
+
+const int ledPin = 5;
+
+const char index_html[] PROGMEM = R"rawliteral(<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blinds</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Blinds</h1>
+    <p>
+        <a href="/on"><button class="button">ON</button></a>
+        <a href="/off"><button class="button button2">OFF</button></a>
+    </p>
+</body>
+</html>)rawliteral";
+
+AsyncWebServer server(80);
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "404: Not found");
+};
 
 void setup() {
-  pinMode(PIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  SPIFFS.begin();
+
   Serial.begin(115200);
   delay(10);
   Serial.println('\n');
-  
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.print("Pripojovani k siti ");
+  Serial.print("Connecting to ");
   Serial.print(ssid); Serial.println(" ...");
 
   int i = 0;
@@ -21,16 +55,29 @@ void setup() {
   }
 
   Serial.println('\n');
-  Serial.println("Pripojeno");  
-  Serial.print("IP adresa:\t");
+  Serial.println("Connected");  
+  Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
+
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", index_html);
+  });
+
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, HIGH);    
+    request->send(200, "text/html", index_html);
+  });
+
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, LOW);    
+    request->send(200, "text/html", index_html);
+  });
+
+  server.onNotFound(notFound);
+  server.begin();
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-      digitalWrite(PIN, HIGH);
-  }
-  else if (WiFi.status() != WL_CONNECTED) {
-      digitalWrite(PIN, LOW);
-  }
+
 }
