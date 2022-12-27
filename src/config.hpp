94ee89a40768468e8c46aa25ac162ba1e267f0b9
pavel kosnar <66769431+PavelKosnar@users.noreply.github.com>
2022-12-27 23:10:09 +0100

    Dokončení naklápění

--- src/config.hpp
+++ src/config.hpp
@@ -1,3 +1,3 @@
-const char* ssid        = "WIFI_NAME";                  //Type your Wi-Fi name instead
-const char* password    = "WIFI_PASSWORD";              //Type your Wi-Fi password instead
-const char* mqtt_broker = "YOUR_IP_ADDRESS";            //Paste your computers ip address
+const char* ssid        = "WIFI_NAME";               //Type your Wi-Fi name instead
+const char* password    = "WIFI_PASSWORD";           //Type your Wi-Fi password instead
+const char* mqtt_broker = "YOUR_IP_ADDRESS";         //Paste your computers ip address
