e8a9f1c355fee516c1003f6583a13d8e783eb9ab
PavelKosnar <kosnarpaveljr@gmail.com>
2023-01-08 20:16:16 +0100

    Úprava stránky

--- data/script.js
+++ data/script.js
@@ -1,26 +1,16 @@
-$(window).on('load', function(){
-    var progress = localStorage.getItem("progress")
-    if (progress > 0) {
-        document.getElementById("progress").value = progress;
-    }
-    else {
-        document.getElementById("progress").value = 0;
-    }
-});
+$(document).ready(function(){
+    $(".home").on('click', function() {
+        var url = window.location.origin;
+        window.location = url;
+    });
 
-$(".home").on('click', function() {
-    var url = window.location.origin;
-    window.location = url;
-});
-
-$("#up").on('click', function() {
-    document.getElementById("state").innerHTML = "UP";
-    localStorage.setItem("progress", "100");
-});
-$("#down").on('click', function() {
-    document.getElementById("state").innerHTML = "DOWN";
-    localStorage.setItem("progress", "0");
-});
-$("#stop").on('click', function() {
-    document.getElementById("state").innerHTML = "STOPPED";
+    $("#up").on('click', function() {
+        $("#blinds-img").hide(10000);
+    });
+    $("#down").on('click', function() {
+        $("#blinds-img").show(10000);
+    });
+    $("#stop").on('click', function() {
+        $("#blinds-img").stop();
+    });
 });
